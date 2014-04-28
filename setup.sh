2eff6413e4fc0a8134b16d88e32fa4416e0480ae
jevi <arnunezm@yahoo.com>
2014-04-27 20:09:19 -0500

    get default custom.el

--- setup.sh
+++ setup.sh
@@ -2,4 +2,8 @@ echo "Setting up Tern"
 cd ~/.emacs.d/site-lisp/tern
 npm install
 
+echo "Download default custom.el"
+cd ~/.emacs.d/
+wget https://raw.githubusercontent.com/arnm/emacs.d/6864f27f5f7e1c1be1b0382fd2f7e32b8d3306bc/custom.el
+
 echo "Setup complete"
