15187ce348c06009a60bbc6afd34cba543341021
arnm <anunez@sparkcognition.com>
2014-05-23 13:29:18 -0500

    pulling down org-mode

--- setup.sh
+++ setup.sh
@@ -2,6 +2,11 @@ echo "Setting up Tern"
 cd ~/.emacs.d/site-lisp/tern
 npm install
 
+echo "Download org-mode"
+cd ~/.emacs.d/site-lisp
+git clone git://orgmode.org/org-mode.git
+sudo make install
+
 echo "Download default custom.el"
 cd ~/.emacs.d/
 wget https://gist.githubusercontent.com/arnm/11359580/raw/75ea6208a545a3af500df42aaf00117147d9c60a/custom.el
